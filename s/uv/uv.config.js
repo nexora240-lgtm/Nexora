6b06dff7a6d7f8780d460a095671e5b20bfcd387
Oscar Su <nexoraunblocked@gmail.com>
2026-04-03 22:41:28 -0500

    ghjghjfgjfghjfghj

--- s/uv/uv.config.js
+++ s/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
 	prefix: "/s/ultraviolet/",
-	bare: "https://34.122.213.182",
+	bare: "https://34.122.213.182:8080",
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
 	handler: "/s/uv/uv.handler.js",
